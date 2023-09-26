691202db6070b75eb65cf534c0bf11df43c2beb9
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-09-26 16:21:56 +0000

    python ver

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,17 +4,21 @@ Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
-#veriable for processor temperature
+
+# veriable for processor temperature
 microbit_temp = 0
+
 
 from microbit import *
 
+
 display.clear()
 display.show(Image.HAPPY)
+
 
 while True:
     if button_a.is_pressed():
         display.clear()
         microbit_temp = temperature()
-        display.show('temperature is ' + str(microbit_temp) + ' C.')
+        display.show("Temperature is " + str(microbit_temp) + "C .")
         display.show(Image.YES)
